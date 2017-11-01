48d35c653a68c569d3965d5f938a6ebb40bba2fa
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-01 17:03:44 -0400

    Created create method for the reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,10 +6,18 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
+     @review = Review.new(review_params)
+     @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,5 +27,9 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant)
   end
 end
