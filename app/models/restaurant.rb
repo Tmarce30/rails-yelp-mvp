21a4eefcbbfe57c91267f199ceffac6377e6e630
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-01 16:37:39 -0400

    Filled restaurant controller with create show new and update

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :review
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-
-  get 'reviews/show'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
-
-  get 'restaurants/index'
-
-  get 'restaurants/show'
-
-  get 'restaurants/new'
-
-  get 'restaurants/create'
-
-  get 'restaurants/edit'
-
-  get 'restaurants/update'
-
-  get 'restaurants/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resource :reviews, only: [ :new, :create ]
+  end
 end
