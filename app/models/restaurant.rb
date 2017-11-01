38e635a7bb25771ab710ff61ef28e06b961931bd
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-01 16:56:35 -0400

    Created validations models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,9 @@
 class Restaurant < ApplicationRecord
-  has_many :review
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian", ]
 
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
-  validates :phone_number, presence: true
-  validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+
+  has_many :reviews, dependent: :destroy
 end
